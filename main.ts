20f9d5911c81099804e955bf7613df64aaf16b6e
Vecto <92492351+Kenobiluke@users.noreply.github.com>
2023-11-06 18:06:58 +0800

    Added Groups

--- main.ts
+++ main.ts
@@ -2,23 +2,55 @@ input.onButtonPressed(Button.A, function () {
     mode = 0
 })
 radio.onReceivedString(function (receivedString) {
+    signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
     if (mode == 1) {
-        signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
-        led.plotBarGraph(
-        Math.map(signal, -95, -50, 0, 9),
-        9
-        )
+        if (Group == 1) {
+            if (receivedString == "1") {
+                led.plotBarGraph(
+                Math.map(signal, -95, -50, 0, 9),
+                9
+                )
+            }
+        } else if (Group == 2) {
+            if (receivedString == "2") {
+                led.plotBarGraph(
+                Math.map(signal, -95, -50, 0, 9),
+                9
+                )
+            }
+        } else if (Group == 3) {
+            if (receivedString == "3") {
+                led.plotBarGraph(
+                Math.map(signal, -95, -50, 0, 9),
+                9
+                )
+            }
+        } else {
+            basic.pause(100)
+        }
     }
 })
 input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    if (mode == 1) {
+        if (Group <= 4) {
+            Group += 1
+        } else {
+            Group = 1
+        }
+    }
     mode = 1
+    basic.showString("" + (Group))
 })
 let degrees = 0
 let signal = 0
 let mode = 0
+let Group = 0
+Group = 1
 radio.setTransmitPower(2)
 radio.setGroup(84)
 mode = 1
+basic.showString("" + (Group))
 basic.forever(function () {
     radio.sendString("2")
     basic.pause(200)
