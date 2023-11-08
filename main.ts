cd540280c70ef1880a20385b903682099c491c84
Vecto <92492351+Kenobiluke@users.noreply.github.com>
2023-11-08 13:18:51 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,58 +1,61 @@
 input.onButtonPressed(Button.A, function () {
+    mode = 3
+    basic.showString("M0")
+    basic.pause(200)
     mode = 0
+})
+input.onButtonPressed(Button.AB, function () {
+    setupComplete = 1
+    basic.clearScreen()
 })
 radio.onReceivedString(function (receivedString) {
     signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
     if (mode == 1) {
-        if (Group == 1) {
-            if (receivedString == "1") {
-                led.plotBarGraph(
-                Math.map(signal, -95, -50, 0, 9),
-                9
-                )
-            }
-        } else if (Group == 2) {
-            if (receivedString == "2") {
-                led.plotBarGraph(
-                Math.map(signal, -95, -50, 0, 9),
-                9
-                )
-            }
-        } else if (Group == 3) {
-            if (receivedString == "3") {
-                led.plotBarGraph(
-                Math.map(signal, -95, -50, 0, 9),
-                9
-                )
-            }
+        if (receivedString == "73") {
+            led.plotBarGraph(
+            Math.map(signal, -95, -50, 0, 9),
+            9
+            )
         } else {
-            basic.pause(100)
+            basic.clearScreen()
         }
     }
 })
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     if (mode == 1) {
+        mode = 3
         if (Group <= 4) {
+            radio.sendString("0")
             Group += 1
         } else {
             Group = 1
         }
+        basic.clearScreen()
+        basic.showString("" + (Group))
+        basic.pause(500)
+        basic.clearScreen()
+        mode = 1
+    } else {
+        mode = 3
+        basic.showString("M1")
+        basic.pause(200)
+        mode = 1
     }
-    mode = 1
-    basic.showString("" + (Group))
 })
 let degrees = 0
 let signal = 0
+let setupComplete = 0
 let mode = 0
 let Group = 0
 Group = 1
 radio.setTransmitPower(2)
-radio.setGroup(84)
 mode = 1
+basic.pause(100)
 basic.showString("" + (Group))
 basic.forever(function () {
-    radio.sendString("2")
+    radio.setGroup(Group + 49)
+    radio.sendString("73")
     basic.pause(200)
 })
 basic.forever(function () {
